c6a791766a27f497af16035f43a325e8e0301844
LUCKY05KAVYA <lakshmimayurikavya.n.2021.ad@ritchennai.edu.in>
2026-05-13 14:31:00 +0530

    Add Flutter Web build

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f73bfc4522dd0bc87bbcdb4bb3088082755c5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3553221492"
+    serviceWorkerVersion: "2367184098"
   }
 });
